afa913b356e6fb51474b6dd0926ae6593e89c4de
isaul19 <saulhola348@gmail.com>
2023-07-06 19:56:14 -0500

    added lambda function

---
+++ amplify/backend/function/turboFunctionPython/src/index.py
@@ -0,0 +1,81 @@
+import json
+import openai
+import boto3
+import os
+
+# CONFIG ---------------------
+ENCRYPTED = os.environ['OPENAI_KEY']
+ssm = boto3.client('ssm')
+
+openai.api_key = ssm.get_parameter(
+    Name=ENCRYPTED, WithDecryption=True)['Parameter']['Value']
+
+FOLDER = 'public/sounds'
+S3_BUCKET_NAME = "imageaudio84957-dev"
+
+# PROPS ----------------------
+
+
+def list_s3_sounds():
+    s3 = boto3.resource("s3")
+    my_bucket = s3.Bucket(S3_BUCKET_NAME)
+
+    TOTAL_SOUNDS = []
+    for objects in my_bucket.objects.filter(Prefix=FOLDER):
+        path = objects.key
+        fileName = path.replace('public/sounds/', '')
+
+        if (not fileName):
+            continue
+
+        TOTAL_SOUNDS.append(fileName)
+
+    return TOTAL_SOUNDS
+
+
+sound_data = list_s3_sounds()
+
+MODEL_COMPORT = f"""
+Vas a recibir un string, primero identifica el nombre del animal y sus caracteristicas y retornarlo en inglés, luego de los siguientes datos: {sound_data}, busca el sonido del animal que identificaste. Si no encuentras un animal o sonido solo devuelve un string vacío en su posición, ejemplo, string: "perro corriendo rapido" => "dog running, perro.mp3"
+"""
+
+
+def buildResponse(status, text):
+    response = {}
+    response["text"] = text
+    return {
+        "statusCode": status,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps(response),
+    }
+
+# LAMBDA -----------------------
+
+
+def handler(event, context):
+    data = json.loads(event["body"])
+
+    openai_data = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": MODEL_COMPORT},
+            {"role": "user", "content": "un burro bebe"},
+            {"role": "assistant", "content": "small donkey, BURRO.mp3"},
+            {"role": "user", "content": "caballo corriendo veloz"},
+            {"role": "assistant",
+                "content": "horse running, CABALLO.wav"},
+            {"role": "user", "content": "jajajjaja"},
+            {"role": "assistant", "content": ","},
+            {"role": "user", "content": data['prompt']}
+        ],
+        temperature=0.9,
+        frequency_penalty=0.5,
+        presence_penalty=1.0
+    )
+
+    text = openai_data['choices'][0]['message']["content"]
+    return buildResponse(200, text)
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "openai": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "imageaudioc68c33e4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "turboFunctionPython": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "imageaudio": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
